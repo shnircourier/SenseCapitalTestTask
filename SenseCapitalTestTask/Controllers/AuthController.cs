80fcb980b84934e3cbff307060f884737a55d6a8
shnircourier <enderfun3377@gmail.com>
2023-03-08 01:33:08 +0300

    Добавил авторизацию через токен

--- SenseCapitalTestTask/Controllers/AuthController.cs
+++ SenseCapitalTestTask/Controllers/AuthController.cs
@@ -1,4 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Shared.Entities;
 
 namespace SenseCapitalTestTask.Controllers;
 
@@ -6,15 +11,44 @@ namespace SenseCapitalTestTask.Controllers;
 [Route("api/[controller]")]
 public class AuthController : ControllerBase
 {
-    [HttpPost]
-    public Task<ActionResult<string>> LogIn()
+    private readonly IConfiguration _config;
+
+    public AuthController(IConfiguration config)
     {
-        
+        _config = config;
     }
 
     [HttpPost]
-    public Task<ActionResult<string>> Register()
+    [Route("login")]
+    public async Task<ActionResult<string>> LogIn()
+    {
+        return Ok("dasdasdasd");
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public async Task<ActionResult<string>> Register()
     {
         
+        
+        return Ok("sdadsad");
+    }
+
+    private string CreateToken(User user)
+    {
+        var claims = new List<Claim> { new Claim(ClaimTypes.Name, user.Username) };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Secret"]));
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var token = new JwtSecurityToken(
+            claims: claims, 
+            expires: DateTime.Now.AddSeconds(int.Parse(_config["JWT:ExpiresSeconds"])),
+            signingCredentials: creds);
+
+        var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return jwt;
     }
 }
--- Shared/Entities/User.cs
+++ Shared/Entities/User.cs
@@ -7,4 +7,6 @@ public class User
     public string Username { get; set; }
 
     public string Password { get; set; }
+
+    public string PasswordSalt { get; set; }
 }
